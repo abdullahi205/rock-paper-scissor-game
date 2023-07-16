524ef110316fcedb42c7d9a77e91342f4d6b72ad
azaamdev <azaamabdalla01@gmail.com>
2023-07-16 15:22:12 +0300

    Add event listener

--- index.js
+++ index.js
@@ -1,85 +1,98 @@
 const choice = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
+const winner = 5;
+let result
+const container = document.querySelector('.container')
 
 function getComputerChoice(){
         let computerChoice = choice[Math.floor(Math.random() * choice.length)];
-        console.log(computerChoice);
         return computerChoice;
         
     
  }
 
-function winnerCheck(playerSelection, computerSelection){
-    if(playerSelection === computerSelection){
-        return "Tie";
-    }else if(
-        (playerSelection === "rock" && computerSelection == "scissors") ||
-    (playerSelection === "scissors" && computerSelection == "paper") ||
-    (playerSelection === "paper" && computerSelection == "rock")
-    ){
-        return "player";
-    }else {
-        return "computer";
-    }
-}
-
-function playRound(playerSelection, computerSelection) {
-    const result = winnerCheck(playerSelection, computerSelection);
-    if(result === "Tie"){
-        return "its tie"
-    }else if(result === "player"){
-        return  `you wins! ${playerSelection} beats ${computerSelection} `
-    }else{
-        return `computer wins ${computerSelection} beats ${playerSelection}`
-    }
-}
-
-function getPlayerChoice (){
-    let validatedInput = false;
-    while (validatedInput === false){
-        const options = prompt("rock paper scissors")
-        if(options === null){
-            continue
-        }
-        const optionsInLower = options.toLowerCase();
-        if(choice.includes(optionsInLower)){
-            validatedInput = true;
-            return optionsInLower;
-        }
-    }
+ function playRound(playerSelection, computerSelection){
     
-        
-    
+  if((computerSelection === 'rock' && playerSelection === 'paper') || (computerSelection === 'scissors' && playerSelection === 'rock') || (computerSelection === 'paper'&& playerSelection === 'scissors')){
+    result = 'YOU WIN!'
+  }else if((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection === 'paper') || (computerSelection === 'paper'&& playerSelection === 'rock')){
+    result = 'COMPUTER WINS!'
+  }else if(computerSelection === playerSelection){
+    result = "IT'S A TIE"
+  }
+
+  return result
 }
 
 function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    console.log("Welcome");
-    for(let i = 0; i < 5; i++){
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(computerSelection, playerSelection));
-        console.log("------------------");
-        if(winnerCheck(playerSelection, computerSelection) === "player"){
-            playerScore++;
-        }
-        else if (winnerCheck(playerSelection, computerSelection) === "computer"){
-            computerScore++;
-        }
+ 
+  const buttons = document.querySelectorAll('button');
+  const playerChoice = document.createElement('div')
+  playerChoice.classList.add('playerchoice')
+  container.appendChild(playerChoice)
+
+  const gameResult = document.createElement('div')
+  gameResult.classList.add('gameresult')
+  container.appendChild(gameResult)
+
+  const roundScore = document.createElement('div')
+  roundScore.classList.add('score')
+  container.appendChild(roundScore)
+
+  const gameWinner = document.createElement('div')
+  gameWinner.classList.add('winner')
+  container.appendChild(gameWinner);
+
+  buttons.forEach(button => {
+    button.addEventListener('click', function(e){
+      computerSelection = getComputerChoice();
+      playerSelection = button.id;
+      playerChoice.innerHTML = `You  <img src="images/${playerSelection}-emoji.png"> : <img src="images/${computerSelection}-emoji.png"> Computer `
+      gameResult.textContent = (playRound(playerSelection, computerSelection))
+      getResult()
+      roundScore.textContent = `YOU: ${playerScore} COMPUTER: ${computerScore}`
+      endGame()
+  })
+  });   
+  function getResult(){
+    if (result === 'YOU WIN!') {
+      playerScore++;
+    } else if (result === 'COMPUTER WINS!') {
+      computerScore++;
     }
-    console.log("Game over")
-        if(playerScore > computerScore){
-            console.log('player wins the game');
-        }else if (playerScore < computerScore){
-            console.log("computer wins the game");
-        }else{
-            console.log("game is tie");
-        }
+  }
+
+  function endGame(){
+    if(playerScore === winner){
+      gameWinner.textContent = "You are genius"
+      buttons.forEach(button => {
+        button.disabled = true;
+      })
+      const reloadPage = document.createElement('button')
+      reloadPage.textContent = 'Play Again'
+      reloadPage.classList.add('reload-button')
+      container.appendChild(reloadPage)
+      reloadPage.addEventListener('click', () =>{
+        location.reload()
+      })
+    }else if(computerScore === winner){
+      gameWinner.textContent = "Have another try"
+      buttons.forEach(button => {
+        button.disabled = true;
+      })
+      const reloadPage = document.createElement('button')
+      reloadPage.textContent = 'Play Again'
+      reloadPage.classList.add('reload-button')
+      container.appendChild(reloadPage)
+      reloadPage.addEventListener('click', () =>{
+        location.reload()
+      })
+    }
+  }
 }
 
 
 
 game();
     
-
-  
